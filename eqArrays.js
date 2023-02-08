ab3a03faef274aeab32b0551b46de9f5a750fcf7
cniscoding <asdfa.com>
2023-02-07 18:28:46 -0800

    update to include different length arrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) { //arrays not same number length return false
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) { //loop first array to get index i
     //console.log('array1 :', array1[i], 'array2 :', array2[i]);
     if (array1[i] !== array2[i]) { // check if array1&2[i] are not the same
