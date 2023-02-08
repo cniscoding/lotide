1a29bf730d0bd6c4cc8a943187592bbab69ae385
cniscoding <asdfa.com>
2023-02-07 18:15:17 -0800

    completed eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -7,8 +7,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) { //loop first array to get index i
+    //console.log('array1 :', array1[i], 'array2 :', array2[i]);
+    if (array1[i] !== array2[i]) { // check if array1&2[i] are not the same
+      return false; // if not return false
+    }
+  }
+  return true;
+};
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual('light', 'light');
-assertEqual(1, 1);
-assertEqual(1, 5);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 5], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 5], [1, 2, 3]), false);
