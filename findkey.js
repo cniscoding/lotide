40fabb435a46b0fd80a5479a2b57218ffed1a6f2
cniscoding <asdfa.com>
2023-02-10 02:07:23 -0800

    working call back findkey.js

--- findkey.js
+++ findkey.js
@@ -1,11 +1,15 @@
-const findKey = function(list, callback){
-  for(names in list){
-    console.log('names :', names)
-    console.log('list[names] :', list[names])
-    console.log('callback(names) :', callback(names))
+const findKey = function(list, callback) {
+  for (names in list) {
+    // console.log('names :', names)
+    // console.log('list[names] :', list[names].stars)
+    // console.log('callback(names) :', callback(names))
+    // console.log('test', list.stars)
+    if (callback(list[names])) {
+      console.log('names :', names);
+      return names;
+    }
   }
-
-}
+};
 
 
 
@@ -16,4 +20,4 @@ findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
