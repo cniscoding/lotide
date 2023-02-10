1a293bd6d74b48a420c3e28da72c98e0069498a8
cniscoding <asdfa.com>
2023-02-09 17:10:24 -0800

    completed findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,5 +13,23 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
+const findKeyByValue = (bestTVShowsByGenre, key) => {
+  for (let genre in bestTVShowsByGenre){
+    // console.log('genre >', genre)
+    // console.log('bestTVShowsByGenre[genre] >', bestTVShowsByGenre[genre])
+    if (key === bestTVShowsByGenre[genre]){
+      // console.log('genre' , genre)
+      return genre;
+    }
+  }
+}
+
+
+
+findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine") // comedy
+findKeyByValue(bestTVShowsByGenre, "The Expanse") // sci_fi
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), 'sci_fi');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "SuperMan"), undefined);
