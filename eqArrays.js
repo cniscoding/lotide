773bb51c9c45f95eb5f860e838b411657ac5bba0
cniscoding <asdfa.com>
2023-02-13 18:21:47 -0800

    refactor eqArrays with module.exports and created eqArraysTest with require()

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`⭕️⭕️⭕️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) { //arrays not same number length return false
     return false;
@@ -19,15 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)){
-    console.log(`⭕️⭕️⭕️ Assertion Array Passed: ${actual} === ${expected}`)
-  } else{
-    console.log(`❌❌❌ Assertion Array Failed: ${actual} !== ${expected}`); // failed
-  }
-};
-
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // true);
-assertArraysEqual([1, 2, 5], [1, 2, 3]); // false );
-assertArraysEqual([1, 'a', 3], [1, 'a', 3]); // true);
+module.exports = eqArrays;
