78c6ea6ab1dae305df92350d8d3f702d04f1dcfe
cniscoding <asdfa.com>
2023-02-13 18:11:27 -0800

    refactor tails and include module.exports. also add tailTest.js with require()

--- tail.js
+++ tail.js
@@ -1,26 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`⭕️⭕️⭕️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(words) {
   console.log(words.slice(1, words.length)); // console.log tail
   return words.slice(1, words.length); // return value
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
+module.exports = tail;
 
