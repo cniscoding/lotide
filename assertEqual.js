405d329bca2218fa1a15576d3c77bf1a8e2c1f3a
cniscoding <asdfa.com>
2023-02-13 18:06:16 -0800

    refactor head.js and assertEqual.js to add module.exports and require(). adding new assertEqualtest.js and headTest.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`⭕️⭕️⭕️ Assertion Passed: ${actual} === ${expected}`);
@@ -7,23 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual('light', 'light');
-assertEqual(1, 1);
-assertEqual(1, 5);
-
-/*
-const errorMsg = "the # is not even";
-for (let number = 2; number <= 5; number++) {
-  console.log(`the # is ${number}`);
-  console.assert(number % 2 === 0, '%o', { number, errorMsg });
-}
-// output:
-// the # is 2
-// the # is 3
-// Assertion failed: {number: 3, errorMsg: "the # is not even"}
-// the # is 4
-// the # is 5
-// Assertion failed: {number: 5, errorMsg: "the # is not even"}
-*/
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+// TEST CODE
+const assertEqual = require('../assertEqual')
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual('light', 'light');
+assertEqual(1, 1);
+assertEqual(1, 5);
