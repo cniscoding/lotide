273c9ad16ef178dca9fffa72d0da120e4bbf8212
cniscoding <asdfa.com>
2023-02-13 21:51:44 -0800

    added chai and mocha. refactored code to use the chai library with mocha. added gitignore.

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), "Hello");
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("return 'hello' for ['Hello', 'Lighthouse', 'Labs']",() => {
+    assert.strictEqual(head[["Hello", "Lighthouse", "Labs"], "Hello"])
+  })
+  it("return 'hello' for head[1]",() => {
+    assert.notStrictEqual(head[1], "Hello")
+  })
+});
