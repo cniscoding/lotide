e6f18eacfd3548ccffd8aef974db96ef2caa2df8
cniscoding <asdfa.com>
2023-02-13 22:40:04 -0800

    eslint

--- test/headTest.js
+++ test/headTest.js
@@ -6,12 +6,12 @@ describe("#head", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
   it("return 'hello' for ['Hello', 'Lighthouse', 'Labs']",() => {
-    assert.strictEqual(head[["Hello", "Lighthouse", "Labs"], "Hello"])
-  })
+    assert.strictEqual(head[["Hello", "Lighthouse", "Labs"], "Hello"]);
+  });
   it("return 'hello' for head[1]",() => {
-    assert.notStrictEqual(head[1], "Hello")
-  })
+    assert.notStrictEqual(head[1], "Hello");
+  });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -5,20 +5,18 @@ describe("#tail", () => {
   const words = ["Yo Yo", "Lighthouse", "Labs"];
   it(`returns ['Lighthouse', 'Labs'] for ${words}`, () => {
     assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
-  })
+  });
   it('original array should still have 3 elements', () => {
-    assert.lengthOf(words, 3)
-  })
+    assert.lengthOf(words, 3);
+  });
   const result = tail(["Hello", "Lighthouse", "Labs"]);
   it(`result has the length of 2`, () => {
     assert.lengthOf(result, 2);
-  })
+  });
   it(`result[0] ensure first element is Lighthouse`, () =>{
-    assert.strictEqual(result[0], 'Lighthouse')
-  })
+    assert.strictEqual(result[0], 'Lighthouse');
+  });
   it(`result[1] ensure second element is Labs`, () =>{
-    assert.strictEqual(result[1], 'Labs')
-  })
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+    assert.strictEqual(result[1], 'Labs');
+  });
 });
